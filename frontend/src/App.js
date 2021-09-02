e309f2efc8c091dcb59fae53a6e9f578ff01862e
sonu28shamra99 <sonu28shamra99@gmail.com>
2021-09-02 21:09:51 +0530

    product and rating component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import Product from './components/Product';
 import data from './data';
 function App() {
   return (
@@ -6,50 +6,20 @@ function App() {
       <header className="row">
         <div>
           <a className="brand" href="/">
-            ApnaBaazar.com
+            amazona
           </a>
         </div>
         <div>
           <a href="/cart">Cart</a>
-          <a href="/signin">Sign In </a>
+          <a href="/signin">Sign In</a>
         </div>
       </header>
       <main>
         <div>
           <div className="row center">
             {data.products.map((product) => (
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                  <img
-                    className="medium"
-                    src={product.image}
-                    alt={product.name}
-                  />
-                </a>
-                <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                  </div>
-                  <div className="price">&#8377;{product.price}</div>
-                </div>
-              </div>
+            
+              <Product key={product._id} product={product}></Product>
             ))}
           </div>
         </div>
@@ -58,5 +28,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
