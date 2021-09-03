96e6e45142c7c802f3599baac286c61254fefa16
sonu28shamra99 <sonu28shamra99@gmail.com>
2021-09-04 04:57:28 +0530

    remove from cart action implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
         <header className="row">
           <div>
             <Link className="brand" to="/">
-              ApnaBazaar
+              ApnaBazaar.com
             </Link>
           </div>
           <div>
