7e692ae795b9fc7e29144dc364a0eb33bab9b9d1
sonu28shamra99 <sonu28shamra99@gmail.com>
2021-09-06 02:20:09 +0530

    created sample user and product using mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,34 @@
 import express from 'express';
-import data from './data.js';
+import mongoose from 'mongoose';
+import userRouter from './routers/userRouter.js';
+import productRouter from './routers/productRouter.js';
+
 
 const app = express();
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/ApnaBazaar', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
+
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
+
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
